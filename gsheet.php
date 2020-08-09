c31c9d319184c6664a060371aecc4ca6e1d645a4
rdcravens <rodger.cravens@gmail.com>
2020-08-09 16:24:27 -0500

    Update gsheet.php

--- gsheet.php
+++ gsheet.php
@@ -12,7 +12,7 @@
 <body>
 <?php
 
-require_once (__DIR__.'/vendor/autoload.php');
+require_once (__DIR__.'/src/Google/autoload.php');
 
 //Reading data from spreadsheet.
 $client = new Google_Client();
